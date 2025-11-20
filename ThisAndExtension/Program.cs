d088d704b8d60a6b52c27dad33c6016137b58d2a
Hennsarv <Henn@sarv.ee>
2025-11-20 12:55:12 +0200

    funktsioonid parameetrid ja suunad

--- ThisAndExtension/Program.cs
+++ ThisAndExtension/Program.cs
@@ -11,6 +11,25 @@ namespace ThisAndExtension
         static void Main(string[] args)
         {
             // uus näide 
+
+            Ahaa ahaa = new Ahaa { Yks = 4, Kaks = 7 };
+            Console.WriteLine(ahaa.Sum());
+            Console.WriteLine(Ahaa.SumX(ahaa));
+            
+            // need kaks rida teevad TÄPSELT üht ja sama
+            Console.WriteLine(Ehee.SumY(ahaa));
+            Console.WriteLine(ahaa.SumY());
+
+            //"..\\..\\Program.cs".ReadAllLines().ToList().ForEach(x => Console.WriteLine(x));
+
+            int õ = 7; int ö = 8;
+            Console.WriteLine($"õ={õ} ö={ö}");
+            Ehee.Naljakas(ref õ, ref ö);
+            Console.WriteLine($"õ={õ} ö={ö}");
+            Ehee.Naljakas(ref õ, ref õ);
+            Console.WriteLine($"õ={õ} ö={ö}");
+
+
         }
     }
 
@@ -19,9 +38,22 @@ namespace ThisAndExtension
         public int Yks { get; set; }
         public int Kaks { get; set; }
 
-        public int Sum() => Kaks + Yks;
+        public int Sum() => this.Kaks + this.Yks;
 
         public static int SumX(Ahaa ahaa) => ahaa.Yks + ahaa.Kaks;
+
+    }
+
+    static class Ehee
+    {
+        public static int SumY(this Ahaa ahaa) => ahaa.Yks + ahaa.Kaks;
+
+        public static string[] ReadAllLines(this string filename) => System.IO.File.ReadAllLines(filename);
+
+        public static void Naljakas (ref int a, ref int b)
+        {
+            a++; b++;
+        }
 
     }
 
