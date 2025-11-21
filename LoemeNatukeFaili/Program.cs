ce5b4592c0e43cf478fd7e83e3803033a7c260bc
Hennsarv <Henn@sarv.ee>
2025-11-21 12:28:39 +0200

    läksime üle ekstensionite peale

--- LoemeNatukeFaili/Program.cs
+++ LoemeNatukeFaili/Program.cs
@@ -16,19 +16,26 @@ namespace LoemeNatukeFaili
             string prodFile = @"..\..\Data\products.csv";
             string[] read = File.ReadAllLines(prodFile);
 
+            var readList = read.ToList();
 
-            var nimed = read[0].Split(',');
-            int iNimetus = 0;
-            int iHind = 0;
-            int iKogus = 0;
-            //int iCatid = 0;
-            for (int j = 0; nimed.Length > j; j++) 
-            {
-                if (nimed[j].Contains("ProductName")) iNimetus = j;
-                if (nimed[j].Contains("UnitPrice")) iHind = j;
-                if (nimed[j].Contains("UnitsInStock")) iKogus = j;
-                //if (nimed[j].Contains("Category")) iCatid= j;
-            }
+
+            #region Jätame vahele
+            // selle jupi võime vahele jätta - me leiame veerunimed sõnastikuga
+            //var nimed = read[0].Split(',');
+            //int iNimetus = 0;
+            //int iHind = 0;
+            //int iKogus = 0;
+            ////int iCatid = 0;
+            //for (int j = 0; nimed.Length > j; j++) 
+            //{
+            //    if (nimed[j].Contains("ProductName")) iNimetus = j;
+            //    if (nimed[j].Contains("UnitPrice")) iHind = j;
+            //    if (nimed[j].Contains("UnitsInStock")) iKogus = j;
+            //    //if (nimed[j].Contains("Category")) iCatid= j;
+            //} 
+            #endregion
+
+            // teeme veerunimedest esimeses reas sõnastiku
             int index = 0;
             var nimedDict = read[0]
                 .Replace(@"""", "")
@@ -39,16 +46,22 @@ namespace LoemeNatukeFaili
 
             //decimal summa = 0;
             //Console.WriteLine("Toode"+new string(' ', 55)+"Hind     "+ "Kogus     ");
-            var tooteList = read.Skip(1)
-                .Where(x => x.Length > 5)
+            var tooteList = read
+                .Skip(1)
+                .Where(x => !string.IsNullOrWhiteSpace(x))    // jäta tühjad read vahele
+                .Where(x => x.Length > 5)    // jäta ära read, pikkusega alla 5
                 .Select(x => x.Replace(@""",", ";").Replace(@"""", ""))
                 .Select(x => x.Split(';'))
+                .Where(x => x.Length > 5)     // jäta vahele read, kus on veerge alla 5
                 .Select(x => new
                 {
-                    Catid = int.TryParse(x[nimedDict["CategoryId"]], out int _cat) ? _cat : 0, 
-                    Nimetus = x[iNimetus],
-                    Hind = decimal.TryParse(x[iHind], out decimal _hind) ? _hind : 0,
-                    Kogus = decimal.TryParse(x[iKogus], out decimal _kogus) ? _kogus : 0,
+                    //Catid = x[nimedDict["CategoryId"]].TryToInt(), //      TryTo<int>(int.TryParse), 
+                    Catid = x.TryColumn("CategoryId", nimedDict).TryToInt(),      // sama mis eelmine kasutades extensionit
+                    Nimetus = x.TryColumn("ProductName", nimedDict),
+                    //Hind = x[iHind].TryToDec(), //     decimal.TryParse(x[iHind], out decimal _hind) ? _hind : 0,
+                    //Kogus = decimal.TryParse(x[iKogus], out decimal _kogus) ? _kogus : 0,
+                    Hind = x.TryColumn("UnitPrice", nimedDict).TryToDec(),
+                    Kogus = x.TryColumn("UnitsInStock", nimedDict).TryToDec()
                 })
                 .ToList();
                 ;
@@ -102,9 +115,28 @@ namespace LoemeNatukeFaili
 
             #endregion
 
-
-
-
         }
     }
+
+    static class E
+    {
+        public delegate bool TryParseHandler<T>(string s, out T value);
+
+        public static int TryToInt(this string s, int def = 0) => int.TryParse(s, out int _i) ? _i : def;
+        public static decimal TryToDec(this string s, decimal def = 0) => decimal.TryParse(s, out decimal _d) ? _d : def;
+    
+        public static T TryTo<T>(this string s, TryParseHandler<T> handler, T def = default) where T : struct
+            => handler(s, out var value) ? value : default;
+
+        public static string TryColumn(this string[] row, string colName, IDictionary<string, int> header, string def = "")
+            => row == null || header == null ? def
+            : row[header[colName]].ToString()                                               // (1) NonSafe
+            //: row[header.TryGetValue(colName, out int _i) ? _i : 0]                       // (2) HalfSafe
+            //: row.ElementAtOrDefault(header.TryGetValue(colName, out int _i) ? _i : 0)    // (3) Safe
+            
+            
+            
+            ;
+    }
+
 }
