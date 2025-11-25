bcfd74375b36642b881967dac12549fdd4737bd3
Hennsarv <Henn@sarv.ee>
2025-11-25 14:26:50 +0200

    Lisasin inimeste editori

---
+++ MVCJSoniga/Controllers/InimesedController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Hosting;
+using MVCJSoniga.Models;
+
+namespace MVCJSoniga.Controllers
+{
+    public class InimesedController : Controller
+    {
+        List<Inimene> inimesed = Inimene.Load(HostingEnvironment.MapPath("~/Content/Inimesed.json"));
+
+        public InimesedController()
+        {
+
+        }
+
+        // GET: Inimesed
+        public ActionResult Index()
+        {
+            return View(inimesed);
+        }
+
+        // GET: Inimesed/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(inimesed.Where(x => x.Id == id).FirstOrDefault());
+        }
+
+        // GET: Inimesed/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inimesed/Create
+        [HttpPost]
+        public ActionResult Create(Inimene inimene)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Inimene.Inimesed.Add(inimene);
+                Inimene.Save();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inimesed/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(inimesed.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: Inimesed/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Inimene edited)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Inimene vana = inimesed.FirstOrDefault(x => x.Id == id);
+                if (vana != null)
+                {
+                    (vana.Name, vana.Vanus) = (edited.Name, edited.Vanus);
+                    Inimene.Save();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inimesed/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            return View(inimesed.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: Inimesed/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                inimesed.Remove(inimesed.FirstOrDefault(x => x.Id == id));
+                Inimene.Save();
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCJSoniga/Models/Inimene.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MVCJSoniga.Models
+{
+    public class Inimene
+    {
+        public Inimene() { this.Id = Inimesed.Count == 0 ? 1 : Inimesed.Max(x => x.Id) + 1; }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Vanus { get; set; }
+
+        static string _filename;// = Server.MapPath("~/Content/Inimesed.json");
+
+        public static List<Inimene> Load(string filename) 
+            => Inimesed = JsonConvert.DeserializeObject<List<Inimene>>(File.ReadAllText(_filename = filename));
+
+        public static List<Inimene> Inimesed = new List<Inimene>();
+
+        public static void Save() => File.WriteAllText(_filename, JsonConvert.SerializeObject(Inimesed));
+
+    }
+}
