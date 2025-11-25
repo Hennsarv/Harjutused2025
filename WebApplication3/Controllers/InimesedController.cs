40d944094be55c039af606ae02bfeb88e3ef592e
Hennsarv <Henn@sarv.ee>
2025-11-25 11:48:19 +0200

    Lisasime paar weebirakendust

---
+++ WebApplication3/Controllers/InimesedController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class InimesedController : Controller
+    {
+        
+
+        // GET: Inimesed
+        public ActionResult Index()
+        {
+            Inimene.Init();
+            return View(Inimene.Inimesed);
+        }
+
+        // GET: Inimesed/Details/5
+        public ActionResult Details(int id)
+        {
+            var inimene = Inimene.Inimesed.Where(x => x.Id == id).SingleOrDefault();
+            return View(inimene);
+        }
+
+        // GET: Inimesed/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inimesed/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Inimene.Inimesed.Add(new Inimene { Nimi = collection["Nimi"], Vanus = int.Parse(collection["Vanus"]) });
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inimesed/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var inimene = Inimene.Inimesed.Where(x => x.Id == id).SingleOrDefault();
+            return View(inimene);
+        }
+
+        // POST: Inimesed/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Inimene editable)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var inimene= Inimene.Inimesed.Where(x => x.Id == id).SingleOrDefault();
+                if (inimene != null) { inimene.Nimi = editable.Nimi; inimene.Vanus = editable.Vanus; }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Inimesed/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Inimesed/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication3/Models/Inimene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Inimene
+    {
+        public Inimene() { Id = 
+                Inimesed.Count == 0 ? 1 :
+                Inimesed.Max(x => x.Id) + 1; }
+
+        public int Id { get; set; }
+        public string Nimi { get; set; }
+        public int Vanus { get; set; }
+
+        public static List<Inimene> Inimesed = new List<Inimene>();
+
+        public static void Init()
+        {
+            if (Inimesed.Count == 0)
+            {
+                Inimesed.Add(new Inimene { Nimi = "Henn", Vanus = 70 });
+                Inimesed.Add(new Inimene { Nimi = "Ants", Vanus = 60 });
+                Inimesed.Add(new Inimene { Nimi = "Peeter", Vanus = 50 });
+                ;
+            }
+        }
+    }
+}
