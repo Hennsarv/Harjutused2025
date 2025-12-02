eabfd920bbef5aeb5b8f8a7d76246f5bf871b9c8
Hennsarv <Henn@sarv.ee>
2025-12-02 11:15:49 +0200

    tegime inimesi ja neile lapsi

---
+++ Dynaamikud/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+
+namespace Dynaamikud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic x;
+
+            x = "hobune";
+            Console.WriteLine(x);
+            x = 77;
+            Console.WriteLine(x.GetType().Name);
+
+            string y = "hobune";
+            Console.WriteLine(y.GetType().Name);
+
+            dynamic Tasku = new Bag();
+
+            Tasku.Nimi = "Henn";
+            Tasku.Vanus = 70;
+
+
+
+
+            Tasku.Vanus++;
+
+            Console.WriteLine($"taskus on {Tasku.Nimi} vanusega {Tasku.Vanus}");
+
+            Punkt p = new Punkt(3, 4);
+            Console.WriteLine(p);
+
+            Punkt p2 = (7, 8);
+            Console.WriteLine(p2);
+
+            p2 += 70;
+
+            
+
+            Console.WriteLine(p+p2+100);
+
+            Inimene i1 = new Inimene() { Nimi = "Henn", Vanus = 70 };
+            Inimene i2 = new Inimene("Peeter Suur", 353);
+            Inimene i3 = ("Toomas Linnupoeg", 44);
+
+            Inimene malle = ("Keegi Malle", 40);
+            Inimene laps = i3 + malle;
+            laps.Nimi = "Kristofer";
+            Console.WriteLine(laps);
+
+
+        }
+    }
+
+    class Bag : DynamicObject
+    {
+        readonly dynamic bag;
+
+        public Bag() => bag = new Dictionary<string, dynamic>();
+
+        public Bag(dynamic dict) => bag = dict;
+
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            switch (bag)
+            {
+                case IDictionary<string, object> d:
+                    result = d.ContainsKey(binder.Name) ? d[binder.Name] : null;
+                    return true;
+                default:
+                    result = bag[binder.Name];
+                    return true;
+            }
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            bag[binder.Name] = value;
+            return true;
+        }
+
+
+
+    }
+
+    class Inimene
+    {
+        public string Nimi { get; set; }
+        public int Vanus { get; set; }
+
+        public Inimene Isa { get; set; }
+        public Inimene Ema { get; set; }
+
+        public Inimene() { }
+
+        public Inimene(string nimi, int vanus) => (Nimi, Vanus) = (nimi, vanus);
+        // {Nimi = nimi; Vanus = vanus;}
+
+        public static implicit operator Inimene((string nimi, int vanus) inimene) => new Inimene(inimene.nimi, inimene.vanus);
+
+
+        public static Inimene operator+ (Inimene isa, Inimene ema) => new Inimene() { Isa = isa, Ema = ema  };
+
+        public override string ToString() => $"inimene {Nimi} isa {Isa.Nimi} ema {Ema.Nimi}";
+
+
+    }
+
+    struct Punkt
+    {
+        int x;
+        int y;
+
+        public Punkt(int x, int y) => (this.x, this.y) = (x, y);
+
+        public override string ToString() => $"{x},{y}";
+
+        public static implicit operator Punkt((int x, int y) p) => new Punkt(p.x, p.y);
+
+        public static implicit operator Punkt(string s) => 
+            string.IsNullOrEmpty(s) ? new Punkt(0,0) :
+            new Punkt(int.TryParse(s.Split(',')[0], out int _x) ? _x : 0,
+                      int.TryParse((s+",").Split(',')[1], out int _y) ? _y : 0);
+
+        public static implicit operator Punkt(int x) => new Punkt(x, 0);
+
+
+        public static Punkt operator+ (Punkt px, Punkt py) => new Punkt(px.x+py.x, px.y+py.y);
+
+    }
+
+}
